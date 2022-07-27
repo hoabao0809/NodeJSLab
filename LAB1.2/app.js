5a174fa170a37bf343bc0fd796e8c0df1ffb595f
hoabao0809 <hoabao.0809@gmail.com>
2022-07-27 14:23:51 +0700

    LAB4.2: controller for page not found

--- LAB1.2/app.js
+++ LAB1.2/app.js
@@ -1,6 +1,8 @@
 const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
+
+const errorController = require('./controllers/error');
 // const expressHbs = require('express-handlebars');
 
 const app = express();
@@ -18,11 +20,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/admin', adminRoutes.router);
 app.use(shopRoutes);
 
-app.use((req, res, next) => {
-  res.status(404).render('404', { pageTitle: 'Page Not Found' });
-
-  // res.sendFile(path.join(__dirname, 'views', '404.html'));
-});
+app.use(errorController.get404);
 
 // const server = http.createServer(app);
 // server.listen(3000);
