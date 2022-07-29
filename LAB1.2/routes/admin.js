c09127096d464e7f6e8eba525064aa759e86daf5
hoabao0809 <hoabao.0809@gmail.com>
2022-07-29 10:35:52 +0700

    LAB4.7: see detail product: retrieve id param from url

--- LAB1.2/routes/admin.js
+++ LAB1.2/routes/admin.js
@@ -1,13 +1,18 @@
 const path = require('path');
+
 const express = require('express');
 
-// const rootDir = require('../util/path');
-const productsController = require('../controllers/product');
+const adminController = require('../controllers/admin');
 
 const router = express.Router();
 
-router.get('/add-product', productsController.getAddProduct);
+// /admin/add-product => GET
+router.get('/add-product', adminController.getAddProduct);
 
-router.post('/add-product', productsController.postAddProduct);
+// /admin/products => GET
+router.get('/products', adminController.getProducts);
 
-module.exports = { router };
+// /admin/add-product => POST
+router.post('/add-product', adminController.postAddProduct);
+
+module.exports = router;
