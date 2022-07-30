aba4ebf308cf94ca39805efd7cb47e5006557fff
hoabao0809 <hoabao.0809@gmail.com>
2022-07-30 10:26:26 +0700

    LAB4.14: Fill data into forms in Edit Mode

--- LAB1.2/controllers/admin.js
+++ LAB1.2/controllers/admin.js
@@ -4,6 +4,7 @@ exports.getAddProduct = (req, res, next) => {
   res.render('admin/edit-product', {
     pageTitle: 'Add Product',
     path: '/admin/add-product',
+    editing: false,
   });
 };
 
@@ -17,15 +18,22 @@ exports.postAddProduct = (req, res, next) => {
   res.redirect('/');
 };
 
-exports.getEDitProduct = (req, res, next) => {
+exports.getEditProduct = (req, res, next) => {
   const editMode = req.query.edit; //important: the extracted data is always string => 'true' instead of true
   if (!editMode) {
     return res.redirect('/');
   }
-  res.render('admin/edit-product', {
-    pageTitle: 'Edit Product',
-    path: '/admin/edit-product',
-    editing: true,
+  const prodId = req.params.productId;
+  Product.findById(prodId, (product) => {
+    if (!product) {
+      return res.redirect('/');
+    }
+    res.render('admin/edit-product', {
+      pageTitle: 'Edit Product',
+      path: '/admin/edit-product',
+      editing: editMode,
+      product: product,
+    });
   });
 };
 
