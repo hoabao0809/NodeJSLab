f312647b9852e6693bb784c5b285862cc449d2cb
hoabao0809 <hoabao.0809@gmail.com>
2022-08-01 13:09:13 +0700

    LAB5.11: inserting data and create a product

--- LAB1.2/models/product.js
+++ LAB1.2/models/product.js
@@ -1,30 +1,58 @@
-const db = require('../util/database');
-const Cart = require('./cart');
+const Sequelize = require('sequelize');
 
-module.exports = class Product {
-  constructor(_id, _title, _imageUrl, _description, _price) {
-    this.id = _id;
-    this.title = _title;
-    this.imageUrl = _imageUrl;
-    this.description = _description;
-    this.price = _price;
-  }
+const sequelize = require('../util/database');
 
-  save() {
-    return db.execute(
-      'INSERT INTO products(title, price, imageUrl, description) VALUES (?,?,?,?)',
-      [this.title, this.price, this.imageUrl, this.description]
-    );
-    // NHằm tránh SQL injection
-  }
+const Product = sequelize.define('product', {
+  id: {
+    type: Sequelize.INTEGER,
+    autoIncrement: true,
+    allowNull: false,
+    primaryKey: true,
+  },
+  title: Sequelize.STRING,
+  price: {
+    type: Sequelize.DOUBLE,
+    allowNull: false,
+  },
+  imageUrl: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+  description: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+});
 
-  static deleteById(id) {}
+module.exports = Product;
 
-  static fetchAll() {
-    return db.execute('SELECT * FROM products');
-  }
+// const db = require('../util/database');
+// const Cart = require('./cart');
 
-  static findById(id) {
-    return db.execute('SELECT * FROM products WHERE products.id = ?', [id]);
-  }
-};
+// module.exports = class Product {
+//   constructor(_id, _title, _imageUrl, _description, _price) {
+//     this.id = _id;
+//     this.title = _title;
+//     this.imageUrl = _imageUrl;
+//     this.description = _description;
+//     this.price = _price;
+//   }
+
+//   save() {
+//     return db.execute(
+//       'INSERT INTO products(title, price, imageUrl, description) VALUES (?,?,?,?)',
+//       [this.title, this.price, this.imageUrl, this.description]
+//     );
+//     // NHằm tránh SQL injection
+//   }
+
+//   static deleteById(id) {}
+
+//   static fetchAll() {
+//     return db.execute('SELECT * FROM products');
+//   }
+
+//   static findById(id) {
+//     return db.execute('SELECT * FROM products WHERE products.id = ?', [id]);
+//   }
+// };
