930840bbc54ae7020413b5dc48e9ec76ae679b57
hoabao0809 <hoabao.0809@gmail.com>
2022-08-01 23:13:12 +0700

    1-to-many and many-to-many relations

--- LAB1.2/controllers/shop.js
+++ LAB1.2/controllers/shop.js
@@ -53,31 +53,33 @@ exports.getIndex = (req, res, next) => {
 
 exports.getCart = (req, res, next) => {
   Cart.getCart((cart) => {
-    Product.fetchAll((products) => {
-      const cartProducts = [];
-      for (let product of products) {
-        const cartProductData = cart.products.find(
-          (prod) => prod.id === product.id
-        );
-        if (cartProductData) {
-          cartProducts.push({
-            productData: product,
-            qty: cartProductData.qty,
-          });
+    Product.findAll()
+      .then((products) => {
+        const cartProducts = [];
+        for (let product of products) {
+          const cartProductData = cart.products.find(
+            (prod) => prod.id === product.id
+          );
+          if (cartProductData) {
+            cartProducts.push({
+              productData: product,
+              qty: cartProductData.qty,
+            });
+          }
         }
-      }
-      res.render('shop/cart', {
-        path: '/cart',
-        pageTitle: 'Your Cart',
-        products: cartProducts,
-      });
-    });
+        res.render('shop/cart', {
+          path: '/cart',
+          pageTitle: 'Your Cart',
+          products: cartProducts,
+        });
+      })
+      .catch((err) => console.log(err));
   });
 };
 
 exports.postCart = (req, res, next) => {
   const proId = req.body.productId;
-  Product.findById(proId, (product) => {
+  Product.findByPk(proId, (product) => {
     Cart.addProduct(proId, product.price);
   });
   res.redirect('/cart');
