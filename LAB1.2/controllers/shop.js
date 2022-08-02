84c3565eee90ac44b1d3e701d09de9fd3d59f5be
hoabao0809 <hoabao.0809@gmail.com>
2022-08-02 11:50:22 +0700

    LAB5.27: store cartitems as orderitems

--- LAB1.2/controllers/shop.js
+++ LAB1.2/controllers/shop.js
@@ -1,5 +1,4 @@
 const Product = require('../models/product');
-const Cart = require('../models/cart');
 
 exports.getProducts = (req, res, next) => {
   Product.findAll()
@@ -154,4 +153,29 @@ exports.getCheckout = (req, res, next) => {
     path: '/checkout',
     pageTitle: 'Checkout',
   });
+};
+
+exports.postOrder = (req, res, next) => {
+  req.user
+    .getCart()
+    .then((cart) => {
+      return cart.getProducts();
+    })
+    .then((products) => {
+      return req.user
+        .createOrder()
+        .then((order) => {
+          return order.addProduct(
+            products.map((product) => {
+              product.orderItem = { quantity: product.cartItem.quantity };
+              return product;
+            })
+          );
+        })
+        .catch((err) => console.log(err));
+    })
+    .then(result => {
+      res.redirect('/orders')
+    })
+    .catch((err) => console.log(err));
 };
