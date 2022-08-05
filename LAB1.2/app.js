d7462a6b746e423438078841231b3f69308d666d
hoabao0809 <hoabao.0809@gmail.com>
2022-08-05 15:54:40 +0700

    LAB6.14: create new Users

--- LAB1.2/app.js
+++ LAB1.2/app.js
@@ -5,6 +5,7 @@ const bodyParser = require('body-parser');
 
 const errorController = require('./controllers/error');
 const mongoConnect = require('./util/database').mongoConnect;
+const User = require('./models/user');
 
 const app = express();
 
@@ -18,17 +19,17 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use((req, res, next) => {
-  // User.findById(1)
-  //   .then(user => {
-  //     req.user = user;
-  //     next();
-  //   })
-  //   .catch(err => console.log(err));
+  User.findById('62ecd9f38fb076623736af82')
+    .then((user) => {
+      req.user = user;
+      next();
+    })
+    .catch((err) => console.log(err));
   next();
 });
 
 app.use('/admin', adminRoutes);
-app.use(shopRoutes); 
+app.use(shopRoutes);
 
 app.use(errorController.get404);
 
