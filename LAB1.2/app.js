d1f97e399a74f4f9f373d52fb82a8fd2211b4e80
hoabao0809 <hoabao.0809@gmail.com>
2022-08-06 15:46:51 +0700

    LAB6.17: adding Add to cart function

--- LAB1.2/app.js
+++ LAB1.2/app.js
@@ -21,7 +21,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use((req, res, next) => {
   User.findById('62ee030f8fb076623736af83')
     .then((user) => {
-      req.user = user;
+      req.user = new User(user.name, user.email, user.cart, user._id);
       next();
     })
     .catch((err) => console.log(err));
