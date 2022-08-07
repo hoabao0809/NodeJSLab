93ab28747978500ccd1ae0e09c987fbd426ce985
hoabao0809 <hoabao.0809@gmail.com>
2022-08-07 09:43:25 +0700

    LAB7.1: connecting to mongoDb with mongoose

--- LAB1.2/app.js
+++ LAB1.2/app.js
@@ -2,9 +2,10 @@ const path = require('path');
 
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 const errorController = require('./controllers/error');
-const mongoConnect = require('./util/database').mongoConnect;
+// const mongoConnect = require('./util/database').mongoConnect;
 const User = require('./models/user');
 
 const app = express();
@@ -32,6 +33,11 @@ app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-mongoConnect(() => {
-  app.listen(3000);
-});
+mongoose
+  .connect(
+    'mongodb+srv://hoabao0809:XqImlQMZFzbFZNpo@cluster0.wxy42.mongodb.net/?retryWrites=true&w=majority'
+  )
+  .then((result) => {
+    app.listen(3000);
+  })
+  .catch((err) => console.log(err));
