0288541f5cb41fae72050e03db105395e85421a7
hoabao0809 <hoabao.0809@gmail.com>
2022-09-10 20:32:53 +0700

    lab8.4: configuring cookies

--- LAB1.2/controllers/auth.js
+++ LAB1.2/controllers/auth.js
@@ -8,6 +8,11 @@ exports.getLogin = (req, res, next) => {
 };
 
 exports.postLogin = (req, res, next) => {
-  res.setHeader('Set-Cookie', 'loggedIn=true');
+  res.setHeader('Set-Cookie', 'loggedIn=true; Max-Age=10');
+  // Some pair keys: 
+  // ['Expires= '; 
+    // 'Max-Age= number in seconds'; 
+    // 'Domain= to which cookies should be sent; 'Secure (cookie only be set if page is served via Https); 
+    // HttpOnly: cookie values cant be accessed via client side JS script => avoiding cross-site scripting attacks]
   res.redirect('/');
 };
