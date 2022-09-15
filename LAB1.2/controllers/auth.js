b0090c19dadee050b24f7c26bdc3ae9b1c05b4fa
hoabao0809 <hoabao.0809@gmail.com>
2022-09-15 21:25:54 +0700

    keeping user input

--- LAB1.2/controllers/auth.js
+++ LAB1.2/controllers/auth.js
@@ -42,12 +42,24 @@ exports.getSignup = (req, res, next) => {
     pageTitle: 'Signup',
     isAuthenticated: false,
     errorMessage: message,
+    oldInput: { email: '', password: '', confirmPassword: '' },
   });
 };
 
 exports.postLogin = (req, res, next) => {
   const email = req.body.email;
   const password = req.body.password;
+
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    console.log(errors.array());
+    return res.status(422).render('auth/login', {
+      path: '/login',
+      pageTitle: 'Login',
+      errorMessage: errors.array()[0].msg,
+    });
+  }
+
   User.findOne({ email: email })
     .then((user) => {
       if (!user) {
@@ -86,6 +98,7 @@ exports.postSignup = (req, res, next) => {
       pageTitle: 'Signup',
       isAuthenticated: false,
       errorMessage: errors.array()[0].msg,
+      oldInput: { email, password, confirmPassword: req.body.confirmPassword },
     });
   }
 
