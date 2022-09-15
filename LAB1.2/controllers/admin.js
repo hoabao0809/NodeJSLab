a4ccd33931d31a461048d3ab7ba1b2a618a69063
hoabao0809 <hoabao.0809@gmail.com>
2022-09-15 20:28:01 +0700

    setup and basic validation

--- LAB1.2/controllers/admin.js
+++ LAB1.2/controllers/admin.js
@@ -62,7 +62,7 @@ exports.postEditProduct = (req, res, next) => {
 
   Product.findById(prodId)
     .then((product) => {
-      if (product.userId !== req.user._id) {
+      if (product.userId.toString() !== req.user._id.toString()) {
         return res.redirect('/');
       }
       product.title = updatedTitle;
@@ -73,7 +73,7 @@ exports.postEditProduct = (req, res, next) => {
         console.log('UPDATED PRODUCT!');
         res.redirect('/admin/products');
       });
-    }
+    })
     .catch((err) => console.log(err));
 };
 
@@ -94,7 +94,7 @@ exports.getProducts = (req, res, next) => {
 
 exports.postDeleteProduct = (req, res, next) => {
   const prodId = req.body.productId;
-  Product.deleteOne({_id: prodId, userId: req.user._id})
+  Product.deleteOne({ _id: prodId, userId: req.user._id })
     .then(() => {
       console.log('DESTROYED PRODUCT');
       res.redirect('/admin/products');
--- LAB1.2/controllers/auth.js
+++ LAB1.2/controllers/auth.js
@@ -3,6 +3,7 @@ const crypto = require('crypto');
 const bcrypt = require('bcryptjs');
 const nodemailer = require('nodemailer');
 const sendgridTransport = require('nodemailer-sendgrid-transport');
+const { validationResult } = require('express-validator/check'); // function allows us to gather all the errors
 
 const User = require('../models/user');
 
@@ -78,6 +79,17 @@ exports.postSignup = (req, res, next) => {
   const email = req.body.email;
   const password = req.body.password;
   const confirmPassword = req.body.confirmPassword;
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    console.log(errors.array());
+    return res.status(422).render('auth/signup', {
+      path: '/signup',
+      pageTitle: 'Signup',
+      isAuthenticated: false,
+      errorMessage: errors.array(),
+    });
+  }
+
   User.findOne({ email: email })
     .then((userDoc) => {
       if (userDoc) {
